5053f5a82e04fe9fd8922865c8057970b4551d08
Milan <4k.milan15@gmail.com>
2024-08-28 16:07:51 +0500

    28.08.2024 16.07 Add lightning menu

--- scripts/data.js
+++ scripts/data.js
@@ -1,12 +1,27 @@
 window.dataLoaded = false;
     let title_of_eom = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste fugiat voluptatum quos rerum, repudiandae doloribus dolores pariatur explicabo laborum corporis!';
-    let methodRecomendation = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste fugiat voluptatum quos rerum, repudiandae doloribus dolores pariatur explicabo laborum corporis!';
-    let dropType = 1;
-    let typeOfButtons = 'hexagon'; // Можер быть еще "lightning" и "tiles";
+    let methodRecomendation = 'fghfghfhfg Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste fugiat voluptatum quos rerum, repudiandae doloribus dolores pariatur explicabo laborum corporis!';
+    let dropType = 13;
+    let typeOfButtons = 'lightning'; // Можер быть "hexagon", "lightning", "tiles" или "video";
+    let background_type = dropType; 
+
+    let literaListEOM1 = 
+    `<b>Гонсалес, Р. Ц., Вудс, Р. Е.</b> Цифровая обработка изображений / Р. Ц. Гонсалес, Р. Е. Вудс. — 3-е изд. — М.: Техносфера, 2012. — 1072 с.
+    Рашка, С., Миржалили, В. Python и машинное обучение / С. Рашка, В. Миржалили. — 3-е изд. — Бирмингем: Packt Publishing, 2019. — 770 с.
+    Сзелиски, Р. Компьютерное зрение: алгоритмы и приложения / Р. Сзелиски. — М.: Вильямс, 2011. — 1104 с.
+    Гудфеллоу, И., Бенжио, Й., Курвилль, А. Глубокое обучение / И. Гудфеллоу, Й. Бенжио, А. Курвилль; пер. с англ. — М.: ДМК Пресс, 2018. — 732 с.
+    Бишоп, К. М. Распознавание образов и машинное обучение / К. М. Бишоп; пер. с англ. — М.: ИД «Вильямс», 2007. — 736 с.
+    Сонка, М., Главач, В., Бойл, Р. Обработка изображений, анализ и машинное зрение / М. Сонка, В. Главач, Р. Бойл; пер. с англ. — М.: ИД «Вильямс», 2004. — 770 с.
+    Черемисин Д.Г., Мкртчян В.Р. Использование нейронных сетей в задачах распознавания математических выражений // Международный научный журнал “СИМВОЛ НАУКИ” №12-2 2022 год 34-36 с.
+    Карахтанов А.А. Использование нейронных сетей для распознавания математических выражений // Математическое и компьютерное моделирование в экономике, страховании и управлении рисками, №5 2020 год 86-89 с.
+    Шолле, Ф. Глубокое обучение с использованием Python / Ф. Шолле. — 2-е изд. — Шелтер-Айленд: Manning Publications, 2021. — 544 с.
+    Документация по Tensorflow Keras URL: https://www.tensorflow.org/ (дата обращения: 11.06.2024)
+    Документация по OpenCV URL: https://docs.opencv.org/4.10.0/ (дата обращения: 11.06.2024)`
+
 
     const data = {
         "index_1":{
-            "subtitle": "dfgdgdgdgdgdgdg 1",
+            "subtitle": "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
             "paragraph_1":[
                 {"text": "<b>Lorem ipsum dolor sit amet consectetur adipisicing elit.</b>"},
                 {"image": false, "image_path": "./content/test_image.png"}
@@ -25,47 +40,82 @@ window.dataLoaded = false;
             ]
         },
         "index_2":{
-            "subtitle": "dfgdgdgdgdgdgdg 2",
+            "subtitle": "dfgdgdgd gdgdgdg",
+            "paragraph_1":[
+                {"text": "<b>Lorem ipsum dolor sit amet consectetur adipisicing elit.</b>"},
+                {"image": false, "image_path": "./content/test_image.png"}
+            ],
+            "paragraph_2":[
+                {"text": false},
+                {"image": true, "image_path": "./content/video/test_video.mp4", "img_sign": " "}
+            ],
+            "paragraph_3":[
+                {"text":"<b><i>Lorem ipsum dolor sit amet consectetur</i></b> adipisicing elit."},
+                {"image": true, "image_path": "./content/video/test_video.mp4", "img_sign": " "}
+            ],
+            "paragraph_4":[
+                {"text":" Lorem ipsum dolor sit amet consectetur adipisicing elit."},
+                {"image": false, "image_path": "./content/test_image.png"}
+            ]
         },
         "index_3":{
-            "subtitle": "dfgdgdgdgdgdgdg 3",
+            "subtitle": "dfgdgd gdgdgdgdg",
+            "paragraph_1":[
+                {"text": "<b>Lorem ipsum dolor sit amet consectetur adipisicing elit.</b>"},
+                {"image": false, "image_path": "./content/test_image.png"}
+            ],
+            "paragraph_2":[
+                {"text": false},
+                {"image": true, "image_path": "./content/video/test_video.mp4", "img_sign": " "}
+            ],
+            "paragraph_3":[
+                {"text":"<b><i>Lorem ipsum dolor sit amet consectetur</i></b> adipisicing elit."},
+                {"image": true, "image_path": "./content/video/test_video.mp4", "img_sign": " "}
+            ],
+            "paragraph_4":[
+                {"text":" Lorem ipsum dolor sit amet consectetur adipisicing elit."},
+                {"image": false, "image_path": "./content/test_image.png"}
+            ]
         },
         "index_4":{
-            "subtitle": "dfgdgdgdgdgdgdg 4",
+            "subtitle": "dfgdgdgd gdgdgdg",
         },
         "index_5":{
-            "subtitle": "dfgdgdgdgdgdgdg 5",
+            "subtitle": "dfgdgdgd gdgdgdg",
         },
         "index_6":{
-            "subtitle": "dfgdgdgdgdgdgdg 6",
+            "subtitle": "dfgdgdgd gdgdgdg",
         },
         "index_7":{
-            "subtitle": "dfgdgdgdgdgdgdg 7",
+            "subtitle": "dfgdgdg dgdgdgdg",
         },
         "index_8":{
-            "subtitle": "dfgdgdgdgdgdgdg 8",
-        },
+            "subtitle": "dfgdgd gdgdgdgdg",
+        }
+        ,
         "index_9":{
-            "subtitle": "dfgdgdgdgdgdgdg 9",
-        },
+            "subtitle": "dfgdgdgdgd gdgdg",
+        } ,
         "index_10":{
             "subtitle": "dfgdgdgdgdgdgdg 10",
-        },
-        "index_11":{
-            "subtitle": "dfgdgdgdgdgdgdg 8",
-        },
-        "index_12":{
-            "subtitle": "dfgdgdgdgdgdgdg 9",
-        },
-        "index_13":{
-            "subtitle": "dfgdgdgdgdgdgdg 10",
-        },
-        "index_14":{
-            "subtitle": "dfgdgdgdgdgdgdg 8",
-        },
-        "index_15":{
-            "subtitle": "dfgdgdgdgdgdgdg 9",
         }
+        // ,
+        // "index_11":{
+        //     "subtitle": "dfgdgdgdgdgdgdg 8",
+        // },
+        // "index_12":{
+        //     "subtitle": "dfgdgdgdgdgdgdg 9",
+        // },
+        // "index_13":{
+        //     "subtitle": "dfgdgdgdgdgdgdg 10",
+        // },
+        // "index_14":{
+        //     "subtitle": "dfgdgdgdgdgdgdg 8",
+        // },
+        // "index_15":{
+        //     "subtitle": "dfgdgdgdgdgdgdg 9",
+        // }
+        // ,
         // "index_16":{
         //     "subtitle": "dfgdgdgdgdgdgdg 8",
         // },
@@ -74,16 +124,8 @@ window.dataLoaded = false;
         // },
         // "index_18":{
         //     "subtitle": "dfgdgdgdgdgdgdg 10",
-        // },
-        // "index_19":{
-        //     "subtitle": "dfgdgdgdgdgdgdg 8",
-        // },
-        // "index_20":{
-        //     "subtitle": "dfgdgdgdgdgdgdg 9",
-        // }         
+        // }     
     };
 
-    
     const themesOfEOM1 = Object.values(data).map(item => item.subtitle).join('\n');
-
 window.dataLoaded = true;
--- scripts/main.js
+++ scripts/main.js
@@ -7,19 +7,20 @@ let mainBody = document.getElementById('main_window_eom_one');
 let backgroundColor = document.createElement('div');
 let backgroundImage = document.createElement('div');
 backgroundColor.classList = 'background_color';
-mainBody.appendChild(backgroundColor)
+mainBody.appendChild(backgroundColor);
 backgroundImage.classList = 'background_image';
-mainBody.appendChild(backgroundImage)
-backgroundImage.innerHTML = `<img width="100%" src="../../content/background_one.jpg" alt="background">
-                            <img width="100%" src="../../content/background_one.jpg" alt="background">
-                            <img width="100%" src="../../content/background_one.jpg" alt="background">`;
+mainBody.appendChild(backgroundImage);
+
+var typeOfDrop = `<img width="100%" src="../../content/background_${background_type}.jpg" alt="background">`;
+
+backgroundImage.innerHTML = typeOfDrop;
 
 function waitForData() {
     miniHead.innerHTML = originalTitle;
     if (window.dataLoaded) {
         window.addEventListener('load',() => {
             let windowWidth  = window.innerWidth;
-            if (windowWidth  <=  650)  {
+            if (windowWidth  <=  650){
                 shortenTitle(blackHeader, 30);  // Сокращаем заголовок до 20 символов
             } else {
                 restoreTitle(blackHeader, originalTitle);
@@ -28,7 +29,7 @@ function waitForData() {
 
         window.addEventListener('resize',() => {
             let windowWidth  = window.innerWidth;
-            if (windowWidth  <=  650)  {
+            if (windowWidth  <=  650) {
                 shortenTitle(blackHeader, 30);  // Сокращаем заголовок до 20 символов
             } else {
                 restoreTitle(blackHeader, originalTitle);
@@ -65,7 +66,7 @@ function waitForData() {
     
         const contentWindowLeft = document.createElement('div');
         contentWindowLeft.classList.add('content__div_left');
-        contentWindowLeft.innerHTML = `${methodRecomendation}`
+        contentWindowLeft.innerHTML = `${methodRecomendation}`;
         contentWindow.appendChild(contentWindowLeft);
     
         const literaBtn = document.createElement('button');
@@ -81,7 +82,7 @@ function waitForData() {
         contentWindowRight.classList.add('content__div_right');
         contentWindowRight.innerHTML = `<img src="./../content/drop_logo_${dropType}.png" alt="drop_logo">`;
         contentWindow.appendChild(contentWindowRight);
-    
+
         const themesNameArr = themesOfEOM1.split('\n');
         let numberOfColumns, numberOfRows;
         
@@ -91,16 +92,15 @@ function waitForData() {
         } else if (themesNameArr.length <= 9) {
             numberOfColumns = Math.ceil(themesNameArr.length / 3);
             numberOfRows = Math.ceil(themesNameArr.length / numberOfColumns);
-        
         } else if (themesNameArr.length <= 12) {
             numberOfColumns = 4;
             numberOfRows = 3;
         } else if (themesNameArr.length <= 15) {
             numberOfColumns = 5;
             numberOfRows = 3;
-        } else if (themesNameArr.length <= 16) {
-            numberOfColumns = 4;
-            numberOfRows = 4;
+        } else if (themesNameArr.length <= 18) {
+            numberOfColumns = 6;
+            numberOfRows = 3;
         } else if (themesNameArr.length <= 20) {
             numberOfColumns = 5;
             numberOfRows = 4;
@@ -109,31 +109,132 @@ function waitForData() {
             numberOfRows = Math.ceil(themesNameArr.length / numberOfColumns);
         }
         
-        for (let i = 0; i < numberOfColumns; i++) {
-            let columnsOfHexagon = document.createElement('div');
-            columnsOfHexagon.classList.add('columns__hexagon', 'col_' + i);
-            contentWindowCenter.appendChild(columnsOfHexagon);
-            for (let j = 0; j < numberOfRows; j++) {
-                let hexagonBtn = document.createElement('button');
-                hexagonBtn.classList.add('button__hexagon_type');
-                columnsOfHexagon.appendChild(hexagonBtn);
+        if(typeOfButtons === 'hexagon'){
+            for (let i = 0; i < numberOfColumns; i++) {
+                let columnsOfHexagon = document.createElement('div');
+                columnsOfHexagon.classList.add('columns__hexagon', 'col_' + i);
+                contentWindowCenter.appendChild(columnsOfHexagon);
+                for (let j = 0; j < numberOfRows; j++) {
+                    let hexagonBtn = document.createElement('button');
+                    hexagonBtn.classList.add('button__hexagon_type');
+                    hexagonBtn.classList.add('themes_button');
+                    columnsOfHexagon.appendChild(hexagonBtn);
+                }
             }
+            
+            let allHexagoneBtn = document.querySelectorAll('.button__hexagon_type');
+            allHexagoneBtn.forEach(function(item, index) {
+                if (index < themesNameArr.length) {
+                    item.innerHTML = (index + 1) + '. ' + themesNameArr[index];
+                    item.setAttribute('onclick', `location.href='#'`);
+                } else {
+                    item.classList.add('hidden_block');
+                }
+            });
+
+            let hexagonField = document.querySelector('.content__div_center');
+            // Увеличиваем высоту на 100 пикселей
+            let newHeight = hexagonField.clientHeight + 80;
+            // Применяем новую высоту
+            hexagonField.style.height = newHeight + 'px';
+
+        } else if (typeOfButtons === 'lightning') {
+            contentWindowCenter.classList.add('lightning');
+            let leftColumn = document.createElement('div');
+            leftColumn.classList = 'left_column';
+            contentWindowCenter.appendChild(leftColumn);
+            let rightColumn = document.createElement('div');
+            rightColumn.classList = 'right_column';
+            contentWindowCenter.appendChild(rightColumn);
+
+            // Подсчитываем количество элементов в массиве
+            const totalThemes = themesNameArr.length;
+            // Определяем количество элементов, которые должны попасть в левую колонку
+            const leftColumnCount = Math.floor(totalThemes / 2);
+
+            // Проходим по массиву и создаем кнопки
+            themesNameArr.forEach((theme, index) => {
+                // Создаем кнопку
+                let button = document.createElement('button');
+                button.classList = 'lightning_button themes_button';
+                // Добавляем обработчик клика для кнопки
+                button.onclick = function() {
+                    location.href = '#'; // Здесь нужный URL
+                };
+                button.innerHTML = `<p class="themes_lightning">${index + 1}. ${theme}</p>`;
+
+                // Распределяем по колонкам
+                if (index < leftColumnCount) {
+                    leftColumn.appendChild(button);
+                } else {
+                    rightColumn.appendChild(button);
+                }
+            });
+
+            // Если количество элементов нечетное, последний элемент добавляется в правую колонку
+            if (totalThemes % 2 !== 0) {
+                rightColumn.appendChild(rightColumn.removeChild(rightColumn.lastChild));
+            }
+
+            if (themesNameArr.length <= 10){
+                let lightningButton = document.querySelectorAll('.lightning_button');
+                lightningButton.forEach((item) => {
+                    item.classList.add('big');
+                });
+            } else if (themesNameArr.length <= 15) {
+                let lightningButton = document.querySelectorAll('.lightning_button');
+                lightningButton.forEach((item) => {
+                    item.classList.add('medium');
+                });
+            } else if (themesNameArr.length >= 18) {
+                let lightningButton = document.querySelectorAll('.lightning_button');
+                lightningButton.forEach((item) => {
+                    item.classList.add('small');
+                });
+            } 
+
+        } else if (typeOfButtons === 'tiles') {
+            console.log('tiles')
+
+        } else if (typeOfButtons === 'video') {
+            console.log('video')
+
         }
-        
-        let allHexagoneBtn = document.querySelectorAll('.button__hexagon_type');
-        allHexagoneBtn.forEach(function(item, index) {
-            if (index < themesNameArr.length) {
-                item.innerHTML = (index + 1) + '. ' + themesNameArr[index];
-                item.setAttribute('onclick', `location.href='./pages/page_${index + 1}_index.html'`);
-            } else {
-                item.classList.add('hidden_block');
-            }
-        });
+
+        let mainDivContent = document.querySelector('#contentWrapper')
+        let popUpStructure = 
+        `<div id="popup_litera_1" class="popup disabled">
+                <div class="popup_body">
+                    <div class="popup_content">
+                        <div class="popup_header">
+                            <h3>Список литературы</h3>
+                            <button class="close_popup" onclick="document.querySelector('#popup_litera_1').classList.add('disabled')"><img src="./content/close.svg" alt="close"></button>
+                        </div>
+                    <div class="popup_text" id="popup_text_2">
+                        <ol>
+                            ${literaListEOM1.split('\n').map(item => `<li>${item}</li>`).join('')}
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>`
+        mainDivContent.innerHTML += popUpStructure;
+        let literaBtnOn = document.querySelector('.content__litera_btn');
+        literaBtnOn.onclick = function() {
+            let popupWindow = document.querySelector('#popup_litera_1');
+            popupWindow.classList.remove('disabled');
+        };
+
     } else {
         // Если данные ещё не загружены, ждем и проверяем снова
         setTimeout(waitForData, location.reload(),  50);
-        
     }
+
+    let viewportHeight = window.innerHeight
+    let blackHeaderHeight = document.querySelector('#header').clientHeight
+    document.querySelector('.content__div').style.height = (viewportHeight - blackHeaderHeight*2 +10 ) + 'px';
+
+
 
 }
 
